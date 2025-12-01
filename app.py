28b4c94c1c91b939292887e966327c0b7bf35c2e
Adam Bastin <73455686+AdamBastin@users.noreply.github.com>
2025-12-01 12:29:06 -0500

    Added docker files

--- app.py
+++ app.py
@@ -2,7 +2,4 @@ import os
 
 from bastinweb import create_app
 
-if __name__ == "__main__":
-    env = os.getenv('FLASK_ENV', 'development').lower()
-    app = create_app(f'bastinweb.config.{env.capitalize()}Config')
-    app.run(host='0.0.0.0', port=8000)
+app = create_app()
